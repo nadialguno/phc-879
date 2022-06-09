41db1a6553238f48cc5ecc304bf39da2e4170344
Nadia Angulo <nayita7_5@hotmail.com>
2022-06-09 09:20:10 -0500

    Implement #fill_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def full_name
+    if last_name.nil?
+      first_name.capitalize
+    else
+      "#{first_name.capitalize} #{last_name.capitalize}"
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#full_name returns the capitalized user first and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+
+    assert_equal "John Lennon", user.full_name
+  end
+
+  test "#full_name works with just first_name" do
+    user = User.new(first_name: "john")
+
+    assert_equal "John", user.full_name
+  end
 end
