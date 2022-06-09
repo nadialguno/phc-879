5d596e83bdeca3009f77b74b5167010ac8d1ce70
Nadia Angulo <nayita7_5@hotmail.com>
2022-06-09 10:29:00 -0500

    Headless chrome

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,6 +2,6 @@ require "test_helper"
 Webdrivers::Chromedriver.update
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [800, 600]
-  # driven_by :headless_chrome
+  # driven_by :selenium, using: :chrome, screen_size: [800, 600]
+  driven_by :headless_chrome
 end
